b924ed90178371a5352391e466a2d3f4d4a9c370
c0d33py <rootkill007@gmail.com>
2023-03-17 16:58:14 +0500

    convert changed

--- transcription/transcribe.py
+++ transcription/transcribe.py
@@ -1,18 +1,14 @@
 # import openai
 import os
 import torch
+import whisper
 
 from django.core.files.storage import default_storage
-import whisper
-from pydub import AudioSegment
-from asgiref.sync import sync_to_async
 
 from .models import MediaField
 
-# openai.api_key = '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' # TODO Mine
-# openai.api_key = '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' # TODO BOSS
-# openai.api_key = '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'  # TODO NEW ACCOUNT
-model = whisper.load_model("tiny")
+
+model = whisper.load_model("large")
 
 
 class Transcribe:
@@ -20,26 +16,16 @@ class Transcribe:
     @staticmethod
     def get_audio_file(file):
         path = default_storage.path(file.upload_file.name)
-        if (file.upload_file.name.endswith('.mp4')):
-            audio = AudioSegment.from_file(path, format='mp4')
-            dir_path = os.path.dirname(path)
-            new_file_name = file.upload_file.name.replace('.mp4', '.mp3')
-            new_file_path = os.path.join(dir_path, new_file_name)
-            audio.export(new_file_path, format='mp3')
-            path = new_file_path
-        return path
+        # Create a new file name
+        new_file_name = path.replace('.mp4', '.wav')
 
-    # def transcribe_file(self, file_id):
-    #     file = MediaField.objects.filter(id=int(file_id)).first()
+        # Convert the file
+        os.system(f'ffmpeg -i {path} {new_file_name}')
 
-    #     filepath = os.path.join(f'media{file.upload_file.url}')
-    #     # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
-    #     with open(filepath, "rb") as f:
-    #         transcript = openai.Audio.transcribe("whisper-1", f)
-    #         file.transcript = transcript['text']
-    #         file.save()
+        return new_file_name
 
     def transcribe_file(self, file_id):
+        pass
         file = MediaField.objects.filter(id=int(file_id)).first()
         if not file:
             return None
@@ -47,7 +33,7 @@ class Transcribe:
         # Get the path of the audio file
         audio_file = Transcribe.get_audio_file(file)
 
-        # Transcribe the audio file
+    #     # Transcribe the audio file
         transcription = model.transcribe(
             audio_file,
             fp16=torch.cuda.is_available(),
@@ -59,7 +45,16 @@ class Transcribe:
         file.save()
 
         # Delete the audio file
-        if os.path.exists(audio_file):
-            os.remove(audio_file)
+        os.remove(audio_file)
 
         return file
+
+    # def transcribe_file(self, file_id):
+    #     file = MediaField.objects.filter(id=int(file_id)).first()
+
+    #     filepath = os.path.join(f'media{file.upload_file.url}')
+    #     # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+    #     with open(filepath, "rb") as f:
+    #         transcript = openai.Audio.transcribe("whisper-1", f)
+    #         file.transcript = transcript['text']
+    #         file.save()
