bce2fa658afc57e7ee2f2ebedc5dff376c3c20b4
c0d33py <rootkill007@gmail.com>
2023-03-29 00:07:18 +0500

    working on transaction of channel data

--- transcription/consumers.py
+++ transcription/consumers.py
@@ -20,10 +20,7 @@ class TranscriptConsumer(AsyncJsonWebsocketConsumer):
     def __init__(self):
         super().__init__()
         from .transcribe import Transcribe
-        model_path = "model/whisper-large-v2-ct2-int8_float16/"
-        print("Loading large model...")
-        self.model = WhisperModel(model_path, device="cuda", compute_type="int8")
-        print("Large model loaded!")
+        self.model_path = "model/whisper-large-v2-ct2-int8_float16/"
         self.transcribe = Transcribe()
 
     async def connect(self):
@@ -35,43 +32,48 @@ class TranscriptConsumer(AsyncJsonWebsocketConsumer):
         await self.close()
 
     async def receive_json(self, content):
-        file_id = content['file_id']
 
-        # Get the model dynamically
-        TusFileModel = await sync_to_async(apps.get_model)('django_tus', 'TusFileModel')
+        try:
+            file_id = content['file_id']
+            transcription = content['transcription']
+            cuda = content['cuda']
+        except KeyError as e:
+            print(f"Missing key: {e}")
+            return
 
-        # Get the TusFileModel object corresponding to the given file_id
-        tus_file = await sync_to_async(get_object_or_404)(TusFileModel, guid=file_id)
+        if transcription:
+            # Get cuda device if cuda is true and set the cuda device
+            device = 'cuda' if cuda else 'cpu'
+            model = WhisperModel(self.model_path, device=device, compute_type="int8")
+            print(f"{device.upper()} device set!")
 
-        if tus_file is None:
-            return None
+            # Get the model dynamically
+            TusFileModel = await sync_to_async(apps.get_model)('django_tus', 'TusFileModel')
+            tus_file = await sync_to_async(get_object_or_404)(TusFileModel, guid=file_id)
 
-        # Convert the audio file to a mono WAV audio file
-        audio_data = await sync_to_async(self.transcribe.get_audio_file)(tus_file.uploaded_file.name)
+            if tus_file is None:
+                return None
 
-        # Transcribe the audio file
-        segments, info = self.model.transcribe(audio_data, beam_size=5)
+            # Convert the audio file to a mono WAV audio file
+            audio_data = await sync_to_async(self.transcribe.get_audio_file)(tus_file.uploaded_file.name)
+            segments, info = model.transcribe(audio_data, beam_size=5)
 
-        paragraph = ""
-        # Send the transcription to the client
-        for segment in segments:
-            paragraph += segment.text + " "
+            # Send the transcription to the client
+            paragraph = ""
+            for segment in segments:
+                paragraph += segment.text + " "
 
-        await self.send(
-            json.dumps({'transcript': paragraph})
-        )
+            await self.send(json.dumps({'transcript': paragraph}))
 
-        # Get app model
-        MediaField = await sync_to_async(apps.get_model)('transcription', 'MediaField')
-        # Create a new MediaField object for the transcribed text
-        transcribe_instance = await sync_to_async(MediaField.objects.create)(transcript=paragraph)
+            # Update the TusFileModel object to reference the MediaField object
+            MediaField = await sync_to_async(apps.get_model)('transcription', 'MediaField')
+            transcribe_instance = await sync_to_async(MediaField.objects.create)(transcript=paragraph)
+            tus_file.content_object = transcribe_instance
+            await sync_to_async(tus_file.save)()
 
-        # Update the TusFileModel object to reference the MediaField object
-        tus_file.content_object = transcribe_instance
-        await sync_to_async(tus_file.save)()
+            # Delete the temporary audio file
+            await sync_to_async(os.remove)(audio_data)
 
-        # Delete the temporary audio file
-        await sync_to_async(os.remove)(audio_data)
-
-        # Close the connection
-        await self.close()
+            # Close the connection
+            await self.close()
+            return
